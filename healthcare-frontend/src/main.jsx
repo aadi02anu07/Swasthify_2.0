780984b7747f5f048de0446a68655fcf78c5547f
Aditya Srivastava <aadi02anu07@gmail.com>
2026-05-14 19:55:27 +0530

    Added analytics tracker

--- healthcare-frontend/src/main.jsx
+++ healthcare-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from "@vercel/analytics/react"
 import App from './App'
 import './index.css'
 
@@ -29,6 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           },
         }}
       />
+       <Analytics /> 
     </BrowserRouter>
   </React.StrictMode>
 )
